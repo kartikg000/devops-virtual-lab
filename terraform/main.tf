a1b05506d24e291d3ba9b4ff0d1927836bd386ad
Kartik <kartikgupta202217@gmail.com>
2026-03-15 21:08:11 +0530

    Added K8s and CI/CD Pipeline (Ignored large files)

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "devops_server" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "my-key" # MAKE SURE YOU HAVE A KEY PAIR NAMED THIS IN AWS
   tags = {
     Name = "DevOps-Lab-Server"
